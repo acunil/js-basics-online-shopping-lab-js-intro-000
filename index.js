1ffaea01982c8830cda05d45b0ebfa1ff1ebbcd6
Lucian Amos <locamos@gmail.com>
2019-11-23 16:40:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,18 +48,12 @@ function removeFromCart(item) {
       i++;
     } else if (cart[i].itemName == item){
       n = i;
-      console.log(`${cart[i].itemName} is ${item}. Ending.`);
-      break;
+      cart.splice(cart[n], 1);
+      return viewCart();
     } else if (i == cart.length+1){
       return "That item is not in your cart.";
     }
   }
-  if (Number.isInteger(n)) {
-    cart.splice(cart[n], 1);
-    return viewCart();
-    }else{
-      
-    }
 }
 
 function placeOrder(cardNumber) {
