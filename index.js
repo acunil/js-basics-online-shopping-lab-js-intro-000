b8591b9395f870c75bd52cf00437be1b009ba5ed
Lucian Amos <locamos@gmail.com>
2019-11-23 17:34:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,11 +65,3 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`;
   }
 }
-
-
-addToCart("one");
-addToCart("two");
-addToCart("three");
-addToCart("four");
-addToCart("five");
-addToCart("six");
