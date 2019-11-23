cc8432cdec0d9b21d0a208f1f33b23b65a560b8a
Lucian Amos <locamos@gmail.com>
2019-11-23 17:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ function removeFromCart(item){
 
 
 function placeOrder(cardNumber) {
+  var t = total();
+  
   if(cardNumber === ''){
     return "Sorry, we don't have a credit card on file for you.";
   } else {
